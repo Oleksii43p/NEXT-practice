bd9e442cd7e5d66089652970a1fc3baf980d7a81
tha_scarfaze <thascarfaze@gmail.com>
2026-02-23 18:32:01 +0400

    play with some logik

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,26 @@
+'use client';
+
+import NoteList from '@/components/NoteList/NoteList';
+import { getNotes, Note } from '@/lib/api';
+import { useState } from 'react';
+
 const Notes = () => {
-  return <div>Notes</div>;
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const handleClick = async () => {
+    const response = await getNotes();
+    if (response?.notes) {
+      setNotes(response.notes);
+    }
+  };
+
+  return (
+    <section>
+      <h1>Notes List</h1>
+      <button onClick={handleClick}>Get my notes</button>
+      {notes.length > 0 && <NoteList notes={notes} />}
+    </section>
+  );
 };
 
 export default Notes;
